137191fdd1d955b90dd65ff6975efb5aa8dda33b
Gensitsky <vadim.gensitsky@gmail.com>
2017-12-07 07:27:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Создать необобщенный класс точки в трехмерном
+    пространстве с целочисленными координатами (Point3D),
+    который наследуется от generic-класса Point2D<T>, рас-
+    смотренного в уроке. Реализовать в классе:
+    ■■ конструктор с параметрами, который принимает на-
+    чальные значения для координат точки;
+    ■■ метод ToString();
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomeCase8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
